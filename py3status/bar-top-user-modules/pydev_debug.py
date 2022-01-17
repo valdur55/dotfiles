83e857db5299160d8cd29342ff76d96d2fefc065
Valdur Kana <valdur55@gmail.com>
2022-01-17 21:57:49 +0200

    [py3status] debug-bar and top-bar rename as bar-top bar-debug. bar-top remote debug 777 bar-debug remote debug 6767

--- py3status/bar-top-user-modules/pydev_debug.py
+++ py3status/bar-top-user-modules/pydev_debug.py
@@ -1,5 +1,6 @@
 """
-Attach pydevd debuger on py3status load..
+Attach pydevd compatible debuger on py3status load.
+PyDev.Debugger (used in PyDev, PyCharm and VSCode Python)
 
 @author valdur55 valdur55@gmail.com
 
@@ -8,14 +9,14 @@ SAMPLE OUTPUT
 """
 import pydevd
 
-def connect_to_pycharm():
+def connect_to_pydevd():
     try:
         pydevd.settrace('localhost', port=6767, stdoutToServer=True, stderrToServer=True, suspend=False)
         return "🐛"
     except Exception:
         return "🍀"
 
-init_result = connect_to_pycharm()
+init_result = connect_to_pydevd()
 
 class Py3status:
     """
@@ -26,10 +27,10 @@ class Py3status:
     def _post_config_hoop(self):
         pass
 
-    def _connect_to_pycharm(self):
-        self._connection_result = connect_to_pycharm()
+    def _connect_to_pydevd(self):
+        self._connection_result = connect_to_pydevd()
 
-    def idea_debug(self):
+    def pydev_debug(self):
         return {
             "cached_until": self.py3.CACHE_FOREVER,
             "full_text":   self._connection_result,
@@ -42,7 +43,7 @@ class Py3status:
         except Exception:
             pass
 
-        self._connect_to_pycharm()
+        self._connect_to_pydevd()
 
 
 if __name__ == "__main__":
