100d27171e5572c2586e7ec788d562af8edbbcf4
Valdur Kana <valdur55@gmail.com>
2026-01-02 00:05:10 +0200

    py3status: logger_sandbox add self.py3.LOG_INFO/WRANRING/DEBUG constants

--- py3status/bar-debug-user-modules/logger_sandbox.py
+++ py3status/bar-debug-user-modules/logger_sandbox.py
@@ -29,10 +29,13 @@ class Py3status:
             self.py3.log("DDDDDDDDDDDDDD", level="debug")
         elif button == 2:
             self.py3.log("IIIIIIIIIIIIIII", level="info")
+            self.py3.log("LOG_I_LOG_I", level=self.py3.LOG_INFO)
         elif button == 3:
             self.py3.log("WWWWWWWWWWWWWW", level="warning")
+            self.py3.log("LOG_WWWWWWWWWWWWWW", level=self.py3.LOG_WARNING)
         elif button == 4:
             self.py3.log("EEEEEEEEEEEEEE", level="error")
+            self.py3.log("LOG_EEEEEEEEEEEEEE", level=self.py3.LOG_ERROR)
         elif button == 5:
             self.py3.log("CCCCCCCCCCCCCC", level="critical")
         elif button == 8:
